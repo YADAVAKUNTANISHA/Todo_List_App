1c6cb7553772fb7d517538cd2bd6bfdda9265b4a
YADAVAKUNTANISHA <158076792+YADAVAKUNTANISHA@users.noreply.github.com>
2024-06-16 15:00:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const taskInput=document.getElementById("taskInput");
+const taskList=document.getElementById("taskList");
+
+function addTask(){
+    const taskText=taskInput.value.trim();
+    if(taskText!==""){
+        const listItem=document.createElement("li");
+        listItem.innerHTML=`
+        <span>${taskText}</span>
+        <button onclick="deleteTask(this)">Delete</button>
+        `;
+
+        taskList.appendChild(listItem);
+        taskInput.value="";
+    }
+}
+
+function deleteTask(button){
+    const listItem=button.parentElement;
+    taskList.removeChild(listItem);
+}
